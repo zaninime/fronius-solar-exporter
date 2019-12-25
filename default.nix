71a398cbe0903e37a1abf089a360e03c2b7aecd5
Francesco Zanini <francesco@zanini.me>
2019-12-25 19:43:12 +0100

    Make SOLAR_SITE_POLL_INTERVAL optional in default config

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ let
   mainClass = "me.zanini.froniussolar.Boot";
 in sbt.mkDerivation rec {
   pname = "fronius-solar-exporter";
-  version = "0.4.0";
+  version = "0.4.1";
 
   depsSha256 = "0284wna0kbza8dj698pfav7bfzq4135s2ibxarx5inxi6dqqi0q6";
 
